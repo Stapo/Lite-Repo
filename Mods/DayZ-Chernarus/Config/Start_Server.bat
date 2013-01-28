c258cc50ef4b5b6efb988390337f57c9714fe794
Lional Stapleton <stapo19@hotmail.com>
2013-01-28 15:27:17 +1100

    Phase-3.2 Testing

--- Mods/DayZ-Chernarus/Config/Start_Server.bat
+++ Mods/DayZ-Chernarus/Config/Start_Server.bat
@@ -1,4 +1,4 @@
 @echo off
-"C:\Program Files (x86)\MySQL\MySQL Server 5.5\bin\mysql" --user=root --password=root --host=127.0.0.1 --port=3306 --database=dayz --execute="call pMain()"
+Resources\mysql.exe --user=root --password=root --host=127.0.0.1 --port=3306 --database=dayz --execute="call pMain()"
 start .\Expansion\beta\arma2oaserver.exe -port=2302 -mod=@DayZ;@Server-Chernarus -name=DayZ -config=Config-Chernarus\ServerSettings.cfg -cfg=Config-Chernarus\Arma2Config.cfg -profiles=Config-Chernarus
 exit
