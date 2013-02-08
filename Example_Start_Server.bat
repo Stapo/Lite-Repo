06c2cd5202122a2044ce06a725aa392e1dc0cce0
Lional Stapleton <stapo19@hotmail.com>
2013-02-08 18:09:49 +1100

    Repo Update - v1.1.4
    - Removed Uneeded Batch File

--- Example_Start_Server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-Resources\mysql.exe --user=YOURMYSQLUSERNAME --password=YOURMYSQLPASSWORD --host=127.0.0.1 --port=3306 --database=YOURMYSQLDATABASE --execute="call pMain()"
-start .\Expansion\beta\arma2oaserver.exe -port=2302 -mod=YOURMODS;@Server -name=DayZ -config=Config_YOURMODNAME\ServerSettings.cfg -cfg=Config_YOURMODNAME\Arma2Config.cfg -profiles=Config_YOURMODNAME
-exit
